55ef7f2381b59045b9f36b07916f0a85edec6282
Benjamin-Walsh <151532653+Benjamin-Walsh@users.noreply.github.com>
2024-02-28 23:37:37 +0000

    Add event listener to the start timer button in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
+//Adds event listener to the start timer button so a function will be performed 
+//when the user clicks it.
+document.getElementById("start-timer").addEventListener("click", function() {
 
+});
