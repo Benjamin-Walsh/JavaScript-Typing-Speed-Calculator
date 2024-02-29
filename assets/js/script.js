0b3edef7d72109b2c5792594d71b882de11574c9
Benjamin-Walsh <151532653+Benjamin-Walsh@users.noreply.github.com>
2024-02-29 03:22:30 +0000

    Add js script which increases timer on page by 1 every second
    When the user presses the 'start timer' button, the script is activated, and if the timerOn variable is set to true, the span on the page with the 'timer' id will be incremented by 1 every 1000ms.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
 //Adds event listener to the start timer button so a function will be performed 
 //when the user clicks it.
 document.getElementById("start-timer").addEventListener("click", function() {
+  
+  // The below code increments the timer by 1 every 1000 ms (1 second).
+  let timerOn = true;
+  // The timerOn variable must be true in order for the following
+  // 'if' statement to be executed.
+  
+  const interval = setInterval(function() {
+  // The above code executes the function below at an interval specified below.
+    
+    if (timerOn) {  
+      // Creates a variable called 'time' which is given a value equal to
+      // the text contained within the span with the 'timer' id.
+      let time = document.getElementById("timer").innerText;
+      
+      // Increases the time variable by 1.
+      time++;
 
+      // Sets the text of the span with 'timer' id to the value
+      // of the time variable after it has been increased.
+      document.getElementById("timer").innerText = time;
+    }  
+    
+  // Sets interval to 1000ms. This means there is a 1-second delay between
+  // instances of the function being run.  
+  }, 1000);  
 });
